af733f835c58e3267a1857024538dc9e3304e814
mingang.he <dustgle@gmail.com>
2014-06-17 15:47:53 +0800

    add READMD.md

--- socks.c
+++ socks.c
@@ -7,5 +7,8 @@ struct ss_server_ctx *ss_create_server(uint16_t port)
 	server = calloc(1, sizeof(typeof(*server)));
 	if (server == NULL)
 		return NULL;
+	server->sock_fd = create_server_socket(port);
+	if (server->sock_fd < 0)
+		DIE("create_server_socket failed!");
 	return server;
 }
